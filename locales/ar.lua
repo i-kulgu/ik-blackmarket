b363d71302389b1b942d0ee1e6e1e6016e0c211a
El Vod Kaa <44878760+elvodkaa@users.noreply.github.com>
2022-06-20 18:54:55 +0100

    Create ar.lua

---
+++ locales/ar.lua
@@ -0,0 +1,30 @@
+local Translations = {
+    error = {
+        incorrect_amount = "مبلغ غير صحيح",
+        no_space = "لا يكفي مساحة في المخزون",
+        no_slots = "لا توجد فتحات كافية في المخزون",
+        no_money = "مال غير كاف",
+        cant_give = "لا يمكن إعطاء عنصر",
+    },
+    target = {
+        browse = "تصفح المتجر",
+    },
+    menu = {
+        close = "❌ اغلا",
+        cost = "السعر: $",
+        weight = "الحجم:",
+        confirm = "تأكيد الشراء",
+        cpi = "السعر/المنتوج:",
+        payment_type = "نوع الدفع",
+        cash = "كاش",
+        card = "البنك",
+        amount = "العدد",
+        submittext = "دفع",
+        blackmoney = "اموال قدرة",
+     }
+}
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
