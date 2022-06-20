5aba6596123cefabfed9f5a7afb482c583a90331
Ilker Kulgu <i.kulgu@gmail.com>
2022-06-20 13:05:09 +0200

    Update pt.lua

--- locales/pt.lua
+++ locales/pt.lua
@@ -1,57 +1,29 @@
 local Translations = {
-
     error = {
-
         incorrect_amount = "Quantidade Incorreta",
-
         no_space = "Sem Espaço Suficiente no Inventário",
-
         no_slots = "Sem Slots Suficientes no Inventário",
-
         no_money = "Sem Dinheiro Suficiente",
-
         cant_give = "Não Posso te dar o Item!",
-
     },
-
     target = {
-
         browse = "Navegar pela Loja",
-
     },
-
     menu = {
-
         close = "❌ Fechar",
-
         cost = "Custo: $",
-
         weight = "Peso:",
-
         confirm = "Confirmar Compra",
-
         cpi = "Custo por Item:",
-
         payment_type = "Tipo do Pagamento",
-
         cash = "Dinheiro",
-
         card = "Cartão",
-
         amount = "Quantidade para Comprar",
-
         submittext = "Pagar",
-
         blackmoney = "Dinheiro Sujo",
-
      }
-
 }
-
 Lang = Locale:new({
-
     phrases = Translations,
-
     warnOnMissing = true
-
 })
